9ecf13666bee1bec5f642873d9c9ea28a732ef8a
Cédric Schippers <cecedabro@gmail.com>
2025-06-19 00:36:56 +0200

    Quick update

--- common/navbar.js
+++ common/navbar.js
@@ -1,7 +1,13 @@
-const base = location.hostname === "127.0.0.1" ? "" : "/expression-website";
+const isLocalhost = location.hostname === "127.0.0.1" || location.hostname === "localhost";
 
-fetch(`${base}/common/navbar.html`)
+// Choose which navbar file to fetch
+const navbarPath = isLocalhost 
+  ? "/common/navbar-local.html"  // local version
+  : "/expression-website/common/navbar-github.html";  // GitHub Pages version
+
+fetch(navbarPath)
   .then(response => response.text())
   .then(data => {
     document.body.insertAdjacentHTML('afterbegin', data);
-  });
+  })
+  .catch(err => console.error("Failed to load navbar:", err));
