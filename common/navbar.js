7774a0cf5a512f1a90e9126bfbba06f14d112928
Cédric Schippers <cecedabro@gmail.com>
2025-06-19 00:28:12 +0200

    Quick update

--- common/navbar.js
+++ common/navbar.js
@@ -1,4 +1,6 @@
-fetch('/common/navbar.html')
+const base = location.hostname === "127.0.0.1" ? "" : "/expression-website";
+
+fetch(`${base}/common/navbar.html`)
   .then(response => response.text())
   .then(data => {
     document.body.insertAdjacentHTML('afterbegin', data);
