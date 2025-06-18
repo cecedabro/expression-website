55542b76bacaf2d26006c1c7ffb5567584fce6b5
Cédric Schippers <cecedabro@gmail.com>
2025-06-18 23:44:56 +0200

    Resturctured repo: moved files out of websitecode folder

---
+++ common/navbar.js
@@ -0,0 +1,6 @@
+const base = location.hostname === "127.0.0.1" ? "" : "/expression-website";
+fetch(`${base}/Website Code/common/navbar.html`)
+  .then(response => response.text())
+  .then(data => {
+    document.body.insertAdjacentHTML('afterbegin', data);
+  });
